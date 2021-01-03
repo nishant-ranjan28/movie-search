ff3c5bb2323fc05695eb9201133907ab93d7c32a
nishant-ranjan28 <nishantranjan78@gmail.com>
2021-01-03 23:39:12 +0530

    created movie list component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import MovieList from "./components/MovieList";
 
 function App() {
+  const [movies, setMovies] = useState([
+    {
+      Title: "Guardians of the Galaxy Vol. 2",
+      Year: "2017",
+      imdbID: "tt3896198",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode VI - Return of the Jedi",
+      Year: "1983",
+      imdbID: "tt0086190",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MovieList movies={movies} />
     </div>
   );
 }
---
+++ src/components/MovieList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MovieList = (props) => {
+  return (
+    <>
+      {props.movies.map((movie, index) => (
+        <div>
+          <img src='{movie.Poster}' alt='movie' />
+        </div>
+      ))}
+    </>
+  );
+};
+export default MovieList;
