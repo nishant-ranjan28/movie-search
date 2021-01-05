aaa46108510d54456f51560c79a054dcf2adf30d
nishant-ranjan28 <nishantranjan78@gmail.com>
2021-01-05 18:28:18 +0530

    display movie list, modify UI

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 const MovieList = (props) => {
+  const FavotiteComponent = props.favotiteComponent;
   return (
     <>
       {props.movies.map((movie, index) => (
-        <div>
-          <img src='{movie.Poster}' alt='movie' />
+        <div className='image-container d-flex justify-content-start m-3'>
+          <img src={movie.Poster} alt='movie' />
+          <div className='overlay d-flex align-items-center justify-content'></div>
+          <FavotiteComponent />
         </div>
       ))}
     </>
