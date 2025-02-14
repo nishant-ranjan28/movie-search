ff7384fd70af314b329f8e0711d5e6755053a25f
Nishant Ranjan <nishant.ranjan@roosterinc.com>
2025-02-14 14:19:43 +0530

    Add theme toggle functionality and integrate Font Awesome icons

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import MovieList from "./components/MovieList";
 import MovieListHeading from "./components/MovieListHeading";
 import SearchBox from "./components/SearchBox";
 import MovieDetailsModal from "./components/MovieDetailsModal"; // Import the new component
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState("avengers");
   const [selectedMovie, setSelectedMovie] = useState(null); // State for selected movie details
   const [showModal, setShowModal] = useState(false); // State for modal visibility
+  const [theme, setTheme] = useState("dark"); // State for theme
 
   const getMovieList = useCallback(async () => {
     try {
@@ -73,11 +76,24 @@ function App() {
 
   const handleCloseModal = () => setShowModal(false);
 
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+  };
+
   return (
-    <div className="container-fluid movie-app">
-      <div className="row d-flex align-items-center mt-4 mb-4">
+    <div className={`container-fluid movie-app ${theme}`}>
+      <div className="row d-flex align-items-center mt-4 mb-4 justify-content-between">
         <MovieListHeading heading="Movies" />
-        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+        <div className="d-flex align-items-center search-theme-container">
+          <SearchBox
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+            theme={theme}
+          />
+          <button onClick={toggleTheme} className="theme-toggle-button">
+            <FontAwesomeIcon icon={theme === "dark" ? faSun : faMoon} />
+          </button>
+        </div>
       </div>
 
       <div className="row">
