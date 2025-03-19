cb2e73bf0ebe019bb86853c574767b2d301dc9a3
Nishant Ranjan <nishant.ranjan@roosterinc.com>
2025-03-19 23:39:01 +0530

    Enhance movie list display with card layout and improved styling; replace buttons with interactive movie cards

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,27 +3,25 @@ import React from "react";
 const MovieList = ({ movies, handleMovieClick }) => {
   return (
     <div className="d-flex flex-wrap justify-content-center">
-      {movies.map((movie) => (
-        <button
-          className="image-container d-flex justify-content-start m-3 position-relative"
-          key={movie.id}
+      {movies.map((movie, index) => (
+        <div
+          className="movie-card d-flex flex-column justify-content-start m-3 position-relative"
+          key={index}
           onClick={() => handleMovieClick(movie.id)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter" || e.key === " ") {
-              handleMovieClick(movie.id);
-            }
-          }}
         >
-          <div className="rating-badge imdb-rating">{movie.vote_average}</div>
-          <div className="rating-badge rotten-tomatoes-rating">
-            {movie.rottenTomatoesRating || "N/A"}
-          </div>
           <img
             src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
             alt={movie.title}
-            className="img-fluid"
+            className="img-fluid movie-poster"
           />
-        </button>
+          <div className="movie-info">
+            <h5 className="movie-title">{movie.title}</h5>
+            <div className="rating-badge imdb-rating">{movie.vote_average}</div>
+            <div className="rating-badge rotten-tomatoes-rating">
+              {movie.rottenTomatoesRating || "N/A"}
+            </div>
+          </div>
+        </div>
       ))}
     </div>
   );
