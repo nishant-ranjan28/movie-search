130fe40766af60288e1fd41661bbed4f5a611533
Nishant Ranjan <nishant.ranjan@roosterinc.com>
2025-03-20 15:27:26 +0530

    Refactor index.js to use React 18's createRoot for rendering

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <Router>
     <App />
-  </Router>,
-  document.getElementById("root")
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
