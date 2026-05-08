b53cddbb94327b03f4a75accc9102bb5560c42e3
nishant-ranjan28 <nishantranjan78@gmail.com>
2026-05-08 21:16:19 +0530

    chore: initialize shadcn/ui and add v1 component set
    Author: Nishant Ranjan

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
 export default function App() {
-  return <div className="p-8 text-2xl">Hello Tailwind</div>;
+  return (
+    <div className="p-8 space-y-4">
+      <h1 className="text-2xl">Hello Tailwind</h1>
+      <Button>Click me</Button>
+    </div>
+  );
 }
